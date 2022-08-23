4cf823af06f21149d9f713655991608d9ced2dce
Giorgos Apostolopoulos <s3apostol@gmail.com>
2022-08-23 18:28:32 +0300

    Test again

--- ganga/GangaCore/GPIDev/Credentials/VomsProxy.py
+++ ganga/GangaCore/GPIDev/Credentials/VomsProxy.py
@@ -27,7 +27,7 @@ class VomsProxyInfo(ICredentialInfo):
         Args:
             requirements (ICredentialRequirement): An object specifying the requirements
             check_file (bool): Raise an exception if the file does not exist
-            create (bool): Create the credential file yep
+            create (bool): Create the credential file 
         """
 
         self._shell = None
